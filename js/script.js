3bc207bf544c4822358885619a927c297b770dc0
Vukasin Gostovic <vukasin@gostovic.de>
2021-02-10 23:57:15 +0100

    da

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let i = 0;
 let links = document.getElementsByTagName("nav")[0].querySelectorAll("div");
-links[0].addEventListener("click", ()=>{Load("grad/index.html")});//umesto XXX link za grad
-links[1].addEventListener("click", ()=>{Load("skola/index.html")});//umesto XXX link za skolu
+links[0].addEventListener("click", ()=>{Load("skola/index.html")});//umesto XXX link za skolu
+links[1].addEventListener("click", ()=>{Load("grad/index.html")});//umesto XXX link za grad
 links[2].addEventListener("click", ()=>{Load("drzava/index.html")});//umesto XXX link za drzavu
 
 function Load(link){
