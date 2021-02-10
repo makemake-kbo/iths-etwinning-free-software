04692f632c435220082d37455eab4ce9bb499c67
Vukasin Gostovic <vukasin@gostovic.de>
2021-02-10 23:22:35 +0100

    updateovan landing page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let i = 0;
 let links = document.getElementsByTagName("nav")[0].querySelectorAll("div");
-links[0].addEventListener("click", ()=>{Load("XXX")});//umesto XXX link za grad
-links[1].addEventListener("click", ()=>{Load("XXX")});//umesto XXX link za skolu
-links[2].addEventListener("click", ()=>{Load("XXX")});//umesto XXX link za drzavu
+links[0].addEventListener("click", ()=>{Load("grad/index.html")});//umesto XXX link za grad
+links[1].addEventListener("click", ()=>{Load("skola/index.html")});//umesto XXX link za skolu
+links[2].addEventListener("click", ()=>{Load("drzava/index.html")});//umesto XXX link za drzavu
 
 function Load(link){
     window.location = link;
